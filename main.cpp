4f3f575a92c7b84bf97c07848d88d26929adc223
Unknown <robertoliver360@gmail.com>
2023-03-22 19:17:58 +0000

    Cleaned up for self written stuff

--- main.cpp
+++ main.cpp
@@ -4,37 +4,6 @@
     #include <SFML/Network.hpp>
     #include "queuec.h"
     using namespace std;
-    //class combatant{
-    //public:
-    //    int health;
-    //    string name;
-    //    int check;
-    //    combatant(int hp,string targetname, int opponenthealth){
-    //        health=hp;
-    //        name=targetname;
-    //        check=opponenthealth;
-    //    };
-
-    //    void attack(combatant &target){
-    //        if(rand()%50>20)
-    //        {
-    //          target.health -= rand()%30;
-    //          if(target.health<0)target.health=0;
-    //          std::cout<<name <<" hits their target "<<target.name<<" who is now at "<<target.health<<" HP.\n";
-    //       }
-    //   }
-
-    //     void check2(combatant target2){
-    //         if (check>target2.health){
-    //             check=target2.health;
-    //             std::cout<<name <<"hits their target "<<target2.name<<" who is now at "<<check <<" HP.\n";
-    //         }
-    //
-    //     }
-
-
-    //};
-
     void server(){
         // queue of messages
         // map/list (socket*)
